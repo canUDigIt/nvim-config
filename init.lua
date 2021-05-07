0423aa9b66e1209d5b6c194912a086c55797c9b8
Tracy Brown Jr <tracybrownjr@gmail.com>
2021-05-07 00:46:50 -0500

    Starting to use the new init.lua

---
+++ init.lua
@@ -0,0 +1,131 @@
+local cmd = vim.cmd
+local fn = vim.fn
+local g = vim.g
+local scopes = {o = vim.o, b = vim.bo, w = vim.wo}
+
+local function opt(scope, key, value)
+    scopes[scope][key] = value
+    if scope ~= 'o' then scopes['o'][key] = value end
+end
+
+local function map(mode, lhs, rhs, opts)
+    local options = {noremap = true}
+    if opts then options = vim.tbl_extend('force', options, opts) end
+    vim.api.nvim_set_keymap(mode, lhs, rhs, options)
+end
+
+local execute = vim.api.nvim_command
+local install_path = fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+
+if fn.empty(fn.glob(install_path)) > 0 then
+    fn.system({'git', 'clone', 'https://github.com/wbthomason/packer.nvim', install_path})
+    execute 'packadd packer.nvim'
+end
+
+local packer = require('packer')
+packer.startup(function()
+    use 'wbthomason/packer.nvim'
+
+    use 'neovim/nvim-lspconfig'
+    use 'nvim-lua/lsp-status.nvim'
+    use 'onsails/lspkind-nvim'
+    use 'hrsh7th/nvim-compe'
+    use 'hrsh7th/vim-vsnip'
+    use 'anott03/nvim-lspinstall'
+    use 'nvim-treesitter/nvim-treesitter'
+    use {
+        'nvim-telescope/telescope.nvim',
+        requires = {{'nvim-lua/popup.nvim'}, {'nvim-lua/plenary.nvim'}}
+    }
+
+    use 'bluz71/vim-nightfly-guicolors'
+    use 'bluz71/vim-moonfly-colors'
+    use 'fenetikm/falcon'
+
+    use {'ojroques/nvim-hardline'}
+
+    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
+
+    use 'b3nj5m1n/kommentary'
+end)
+
+-------------------- OPTIONS -------------------------------
+local indent = 4
+cmd 'colorscheme moonfly'                             -- Put your favorite colorscheme here
+opt('b', 'expandtab', true)                           -- Use spaces instead of tabs
+opt('b', 'shiftwidth', indent)                        -- Size of an indent
+opt('b', 'smartindent', true)                         -- Insert indents automatically
+opt('b', 'tabstop', indent)                           -- Number of spaces tabs count for
+opt('o', 'completeopt', 'menuone,noselect')           -- Completion options (for deoplete)
+opt('o', 'hidden', true)                              -- Enable modified buffers in background
+opt('o', 'ignorecase', true)                          -- Ignore case
+opt('o', 'smartcase', true)                           -- Don't ignore case with capitals
+opt('o', 'splitbelow', true)                          -- Put new windows below current
+opt('o', 'splitright', true)                          -- Put new windows right of current
+opt('o', 'termguicolors', true)                       -- True color support
+opt('o', 'wildmode', 'list:longest')                  -- Command-line completion mode
+opt('w', 'list', true)                                -- Show some invisible characters (tabs...)
+opt('w', 'number', true)                              -- Print line number
+opt('w', 'relativenumber', true)                      -- Relative line numbers
+opt('w', 'wrap', false)                               -- Disable line wrap
+
+-------------------- MAPPINGS ------------------------------
+map('', '<leader>c', '"+y')       -- Copy to clipboard in normal, visual, select and operator modes
+map('i', '<C-u>', '<C-g>u<C-u>')  -- Make <C-u> undo-friendly
+map('i', '<C-w>', '<C-g>u<C-w>')  -- Make <C-w> undo-friendly
+
+map('n', '<C-l>', '<cmd>noh<CR>')    -- Clear highlights
+map('n', '<leader>o', 'm`o<Esc>``')  -- Insert a newline in normal mode
+
+require('hardline').setup {}
+require('neogit').setup {}
+
+-------------------- TREE-SITTER ---------------------------
+local ts = require 'nvim-treesitter.configs'
+ts.setup {ensure_installed = 'maintained', highlight = {enable = true}}
+
+-------------------- LSP -----------------------------------
+local lsp = require 'lspconfig'
+lsp.clangd.setup {}
+lsp.cmake.setup {}
+lsp.rust_analyzer.setup{}
+lsp.svelte.setup{}
+lsp.pyright.setup{}
+
+map('n', '<space>,', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>')
+map('n', '<space>;', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>')
+map('n', '<space>a', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+map('n', '<space>d', '<cmd>lua vim.lsp.buf.definition()<CR>')
+map('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+map('n', '<space>h', '<cmd>lua vim.lsp.buf.hover()<CR>')
+map('n', '<space>m', '<cmd>lua vim.lsp.buf.rename()<CR>')
+map('n', '<space>r', '<cmd>lua vim.lsp.buf.references()<CR>')
+map('n', '<space>s', '<cmd>lua vim.lsp.buf.document_symbol()<CR>')
+
+-------------------- COMPLETION ------------------------------
+require 'compe'.setup {
+    enabled = true,
+    source = {
+      path = true,
+      buffer = true,
+      calc = true,
+      nvim_lsp = true,
+      nvim_lua = true,
+      vsnip = true,
+    },
+}
+
+-- <Tab> to navigate the completion menu
+map('i', '<S-Tab>', 'pumvisible() ? "\\<C-p>" : "\\<Tab>"', {expr = true})
+map('i', '<Tab>', 'pumvisible() ? "\\<C-n>" : "\\<Tab>"', {expr = true})
+map('i', '<C-Space>', 'compe#complete()', {expr = true, noremap = true, silent = true})
+map('i', '<CR>', 'compe#confirm("<CR>")', {expr = true, noremap = true, silent = true})
+map('i', '<C-e>', 'compe#close("<C-e>")', {expr = true, noremap = true, silent = true})
+map('i', '<C-f>', 'compe#scroll({ "delta": +4 }', {expr = true, noremap = true, silent = true})
+map('i', '<C-d>', 'compe#scroll({ "delta": -4 }', {expr = true, noremap = true, silent = true})
+
+-------------------- TELESCOPE ------------------------------
+map('n', '<leader>ff', '<cmd>Telescope find_files<cr>', {noremap = true})
+map('n', '<leader>fg', '<cmd>Telescope live_grep<cr>', {noremap = true})
+map('n', '<leader>fb', '<cmd>Telescope buffers<cr>', {noremap = true})
+map('n', '<leader>fh', '<cmd>Telescope help_tags<cr>', {noremap = true})
