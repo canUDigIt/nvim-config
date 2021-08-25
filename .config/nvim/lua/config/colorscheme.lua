f24485fdc37cb1a2fa8bc51ca16c31bdab4e3721
Tracy Brown Jr <tracybrownjr@gmail.com>
2021-08-24 22:16:52 -0500

    Breaking update from nightfox theme

--- .config/nvim/lua/config/colorscheme.lua
+++ .config/nvim/lua/config/colorscheme.lua
@@ -13,4 +13,4 @@ vim.g.material_disable_background = false
 
 vim.api.nvim_set_keymap('n', '<leader>mm', [[<Cmd>lua require('material.functions').toggle_style()<CR>]], { noremap = true, silent = true })
 
-require('nightfox').set()
+require('nightfox').load()
