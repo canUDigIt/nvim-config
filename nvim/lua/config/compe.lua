ed5d7bfcca457304d0f9667475fc111d854b9ae5
Tracy Brown Jr <tracybrownjr@gmail.com>
2021-10-06 19:50:27 -0500

    Don't want tab to complete

--- nvim/lua/config/compe.lua
+++ nvim/lua/config/compe.lua
@@ -5,14 +5,6 @@ local utils = require('utils')
 vim.cmd [[set shortmess+=c]]
 utils.opt('o', 'completeopt', 'menuone,noselect')
 
-local has_words_before = function()
-  if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then
-    return false
-  end
-  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-end
-
 local feedkey = function(key)
   vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes(key, true, true, true), "n", true)
 end
@@ -42,8 +34,6 @@ cmp.setup {
                 feedkey("<C-n>")
             elseif luasnip.expand_or_jumpable() then
                 luasnip.expand_or_jump()
-            elseif has_words_before() then
-                cmp.complete()
             else
                 fallback() -- The fallback function sends a already mapped key. In this case, it's probably `<Tab>`.
             end
