3247ba023744082f324836d2d61fe54604b1764d
Tracy Brown Jr <tracybrownjr@gmail.com>
2023-02-08 14:12:00 -0600

    Adding in neotree. Removing alpha plugin

--- nvim/lua/config/init.lua
+++ nvim/lua/config/init.lua
@@ -97,7 +97,7 @@ dap.configurations.cpp = {
     type = 'codelldb',
     request = 'launch',
     program = function()
-      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+      return vim.fn.input({'Path to executable: ', vim.fn.getcwd() .. '/', 'file'})
     end,
     cwd = '${workspaceFolder}',
     stopOnEntry = false,
