c9e265c20cb9c532ba759f0b6f1c897a20f0e171
Tracy Brown Jr <tracybrownjr@gmail.com>
2023-07-14 21:51:52 -0500

    Using lldb-vscode for main debugger

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -75,11 +75,42 @@ dap.listeners.after.event_exited['dapui_config'] = function()
   dapui.close{}
 end
 
-dap.adapters.cppdbg = {
-  id = 'cppdbg',
+dap.adapters.lldb = {
+  name = 'lldb',
   type = 'executable',
-  command = vim.fn.stdpath("data") .. "/mason/bin/OpenDebugAD7",
+  command = '/usr/bin/lldb-vscode',
 }
-require('dap.ext.vscode').load_launchjs(nil, { cppdbg = {'c', 'cpp', 'rust'} })
+
+dap.configurations.cpp = {
+  {
+    name = 'Launch',
+    type = 'lldb',
+    request = 'launch',
+    program = function()
+      return vim.fn.input('Path to executable: ', vim.fn.getcwd() .. '/', 'file')
+    end,
+    cwd = '${workspaceFolder}',
+    stopOnEntry = false,
+    args = {},
+
+    -- 💀
+    -- if you change `runInTerminal` to true, you might need to change the yama/ptrace_scope setting:
+    --
+    --    echo 0 | sudo tee /proc/sys/kernel/yama/ptrace_scope
+    --
+    -- Otherwise you might get the following error:
+    --
+    --    Error on launch: Failed to attach to the target process
+    --
+    -- But you should be aware of the implications:
+    -- https://www.kernel.org/doc/html/latest/admin-guide/LSM/Yama.html
+    -- runInTerminal = false,
+  },
+}
+
+-- If you want to use this for Rust and C, add something like this:
+
+dap.configurations.c = dap.configurations.cpp
+dap.configurations.rust = dap.configurations.cpp
 
 require('dap-python').setup('~/.virtualenvs/debugpy/bin/python')
