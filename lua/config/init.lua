b5671527a2273b7109e69c36b39c327594b04e96
Tracy Brown Jr <tracybrownjr@gmail.com>
2024-01-13 22:59:44 -0600

    Adding noice.nvim and using dropdown for telescope

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -61,6 +61,27 @@ require('nvim-treesitter.configs').setup {
 require('telescope').load_extension('zf-native')
 require('telescope').load_extension('lazy')
 
+-- noice
+
+require("noice").setup({
+  lsp = {
+    -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
+    override = {
+      ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+      ["vim.lsp.util.stylize_markdown"] = true,
+      ["cmp.entry.get_documentation"] = true,
+    },
+  },
+  -- you can enable a preset for easier configuration
+  presets = {
+    bottom_search = true, -- use a classic bottom cmdline for search
+    command_palette = true, -- position the cmdline and popupmenu together
+    long_message_to_split = true, -- long messages will be sent to a split
+    inc_rename = false, -- enables an input dialog for inc-rename.nvim
+    lsp_doc_border = false, -- add a border to hover docs and signature help
+  },
+})
+
 -- debugging
 local dap = require('dap')
 local dapui = require('dapui')
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -102,5 +102,20 @@ return {
   'mfussenegger/nvim-dap-python',
   'ziglang/zig.vim',
   'Civitasv/cmake-tools.nvim',
-  'p00f/clangd_extensions.nvim'
+  'p00f/clangd_extensions.nvim',
+  {
+    "folke/noice.nvim",
+    event = "VeryLazy",
+    opts = {
+      -- add any options here
+    },
+    dependencies = {
+      -- if you lazy-load any plugin below, make sure to add proper `module="..."` entries
+      "MunifTanjim/nui.nvim",
+      -- OPTIONAL:
+      --   `nvim-notify` is only needed, if you want to use the notification view.
+      --   If not available, we use `mini` as the fallback
+      "rcarriga/nvim-notify",
+    }
+  }
 }
