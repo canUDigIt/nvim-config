8ce778e2c534050acc06ea82eb92a37090287ab9
Tracy Brown Jr <tracybrownjr@gmail.com>
2024-07-15 08:22:17 -0500

    Updating keymapping to v3 which-key spec

--- lua/keymappings.lua
+++ lua/keymappings.lua
@@ -2,40 +2,32 @@ local builtin = require('telescope.builtin')
 local themes = require('telescope.themes')
 
 local wk = require('which-key')
-wk.register({
-  ['<c-l>'] = { '<cmd>noh<cr>', 'No Highlight' },
+wk.add({
+  { "<c-l>", "<cmd>noh<cr>", desc = "No Highlight" },
 })
 
 -- Searching keys
-wk.register({
-  ['<leader>'] = {
-    ['?'] = { function() builtin.oldfiles() end, '[?] Find recently opened files' },
-    ['<space>'] = { function() builtin.buffers() end, '[ ] Find existing buffers' },
-    ['/'] = { function()
+wk.add({
+  { "<leader>/", function()
       -- You can pass additional configuration to telescope to change theme, layout, etc.
       builtin.current_buffer_fuzzy_find(themes.get_dropdown {
         winblend = 10,
         previewer = false,
       })
-    end, '[/] Fuzzily search in current buffer]' },
-    s = {
-      name = 'search',
-      f = { function () builtin.find_files(themes.get_dropdown()) end, '[S]earch [F]iles' },
-      h = { function() builtin.help_tags(themes.get_dropdown()) end, '[S]earch [H]elp' },
-      w = { function () builtin.grep_string(themes.get_dropdown()) end, '[S]earch current [W]ord' },
-      g = { function () builtin.live_grep(themes.get_dropdown()) end, '[S]earch by [G]rep' },
-      d = { function ()builtin.diagnostics(themes.get_dropdown()) end, '[S]earch [D]iagnostics' },
-    },
-  },
+    end, desc = "[/] Fuzzily search in current buffer]" },
+  { "<leader><space>", function() builtin.buffers() end, desc = "[ ] Find existing buffers" },
+  { "<leader>?", function() builtin.oldfiles() end, desc = "[?] Find recently opened files" },
+  { "<leader>s", group = "Search" },
+  { "<leader>sd", function () builtin.diagnostics(themes.get_dropdown()) end, desc = "[S]earch [D]iagnostics" },
+  { "<leader>sf", function () builtin.find_files(themes.get_dropdown()) end, desc = "[S]earch [F]iles" },
+  { "<leader>sg", function () builtin.live_grep(themes.get_dropdown()) end, desc = "[S]earch by [G]rep" },
+  { "<leader>sh", function () builtin.help_tags(themes.get_dropdown()) end, desc = "[S]earch [H]elp" },
+  { "<leader>sw", function () builtin.grep_string(themes.get_dropdown()) end, desc = "[S]earch current [W]ord" },
 })
 
 -- Version control
-wk.register({
-  ['<leader>'] = {
-    g = {
-      name = 'Git',
-      s = {  '<cmd>Neogit<cr>', 'Git Status' },
-    },
-    h = { name = 'Hunk' }
-  },
-})
+wk.add({
+  { "<leader>g", group = "Git" },
+  { "<leader>gs", "<cmd>Neogit<cr>", desc = "Git Status" },
+  { "<leader>h", group = "Hunk" },
+})  
