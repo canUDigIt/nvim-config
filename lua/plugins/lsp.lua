759c0d7bbbe094bb83a968c186a5d1321e474f2b
Tracy Brown Jr <tracybrownjr@gmail.com>
2024-12-08 00:19:09 -0600

    Removing custom keymaps in favor of defaults

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -49,18 +49,10 @@ vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist, { desc = 'Diagnostics
 vim.api.nvim_create_autocmd('LspAttach', {
   group = vim.api.nvim_create_augroup('UserLspConfig', {}),
   callback = function(ev)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.bo[ev.buf].omnifunc = 'v:lua.vim.lsp.omnifunc'
-
     -- Buffer local mappings.
     -- See `:help vim.lsp.*` for documentation on any of the below functions
     local opts = { buffer = ev.buf }
     vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, vim.tbl_extend('keep', opts, { desc = '[LSP] Declaration' }))
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, vim.tbl_extend('keep', opts, { desc = '[LSP] Definition' }))
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, vim.tbl_extend('keep', opts, { desc = '[LSP] Hover' }))
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, vim.tbl_extend('keep', opts, { desc = '[LSP] Implementation' }))
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help,
-      vim.tbl_extend('keep', opts, { desc = '[LSP] Signature Help' }))
     vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder,
       vim.tbl_extend('keep', opts, { desc = '[LSP] Add Workspace Folder' }))
     vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder,
@@ -70,13 +62,6 @@ vim.api.nvim_create_autocmd('LspAttach', {
     end, vim.tbl_extend('keep', opts, { desc = '[LSP] List Workspace Folders' }))
     vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition,
       vim.tbl_extend('keep', opts, { desc = '[LSP] Type Definition' }))
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, vim.tbl_extend('keep', opts, { desc = '[LSP] Rename' }))
-    vim.keymap.set({ 'n', 'v' }, '<space>ca', vim.lsp.buf.code_action,
-      vim.tbl_extend('keep', opts, { desc = '[LSP] Code Action' }))
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, vim.tbl_extend('keep', opts, { desc = '[LSP] References' }))
-    vim.keymap.set('n', '<space>=', function()
-      vim.lsp.buf.format { async = true }
-    end, vim.tbl_extend('keep', opts, { desc = '[LSP] Format' }))
   end,
 })
 
