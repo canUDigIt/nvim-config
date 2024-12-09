622288b070aeb12e630e21e7692df155ef32b60b
Tracy Brown Jr <tracybrownjr@gmail.com>
2024-12-08 20:25:38 -0600

    Show highlighting by default

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,6 +3,9 @@ return {
     'nvim-treesitter/nvim-treesitter',
     opts = {
         ensure_installed = { "c", "cpp", "cmake", "lua", "markdown", "markdown_inline", "odin", "python", "rust", "typescript", "vim", "zig" },
+        highlight ={
+          enable = true,
+        },
         textobjects = {
           select = {
             enable    = true,
