871bbb09d39692aac36231d68552973821a58961
Tracy Brown <tracy.m.brown@navicogroup.com>
2025-01-07 08:53:41 -0600

    Adding back in neogit so I don't to use lazygit as much

---
+++ lua/plugins/neogit.lua
@@ -0,0 +1,17 @@
+return {
+  "NeogitOrg/neogit",
+  opts = {
+    integrations = {
+      diffview = true,
+      mini_pick = true,
+    }
+  },
+  config = function(opts)
+    require("neogit").setup(opts)
+    vim.keymap.set('n', '<leader>ng', ':Neogit<cr>', { desc = '[N]eo[G]it' })
+  end,
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "sindrets/diffview.nvim",
+  }
+}
