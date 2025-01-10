96d4614b53c1e11135f545ad85c3b899685d8264
Tracy Brown Jr <tracybrownjr@gmail.com>
2025-01-09 21:33:39 -0600

    Removing a lot of plugins for snacks and mini nvim

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -3,3 +3,14 @@ vim.keymap.set('n', '<c-l>', '<cmd>noh<cr>', { desc = 'No Highlight' })
 vim.keymap.set('n', '<leader>xG', '<cmd>source %<cr>')
 vim.keymap.set('n', '<leader>xl', ':.lua<cr>')
 vim.keymap.set('v', '<leader>xl', ':lua<cr>')
+
+function _G.set_terminal_keymaps()
+  local map_opts = {buffer = 0}
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], map_opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], map_opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], map_opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], map_opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], map_opts)
+end
+
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,22 @@
+return {
+  "folke/snacks.nvim",
+  priority = 1000,
+  lazy = false,
+  ---@type snacks.Config
+  opts = {
+    bigfile = { enabled = true },
+    input = { enabled = true },
+    lazygit = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    terminal = { enabled = true },
+  },
+  keys = {
+    { "<leader>gf", function() Snacks.lazygit.log_file() end, desc = "Lazygit Current File History" },
+    { "<leader>gg", function() Snacks.lazygit() end, desc = "Lazygit" },
+    { "<leader>gl", function() Snacks.lazygit.log() end, desc = "Lazygit Log (cwd)" },
+    { "<leader>un", function() Snacks.notifier.hide() end, desc = "Dismiss All Notifications" },
+    { "<c-/>",      function() Snacks.terminal() end, desc = "Toggle Terminal" },
+  }
+}
