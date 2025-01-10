dff1cbc5fa941c44f485d4b50ae178081e581068
Tracy Brown <tracy.m.brown@navicogroup.com>
2025-01-10 09:21:48 -0600

    Mapping clashed with parameter swapping

--- lua/plugins/arrow.lua
+++ lua/plugins/arrow.lua
@@ -1,7 +1,7 @@
 return {
   "otavioschwanck/arrow.nvim",
   opts = {
-    leader_key = '<leader>a', -- Recommended to be a single key
+    leader_key = '\\', -- Recommended to be a single key
     buffer_leader_key = '<leader>m', -- Per Buffer Mappings
   }
 }
