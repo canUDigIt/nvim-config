c790003a9e9e78e0317b60ff614c6bd1c8bdc748
Tracy Brown Jr <tracybrownjr@gmail.com>
2025-01-20 20:29:01 -0600

    I use enough mini modules to just use the whole thing

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -1,104 +1,66 @@
 return {
-  {
-    'echasnovski/mini.ai',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.align',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.bracketed',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.clue',
-    config = function()
-      local miniclue = require('mini.clue')
-      miniclue.setup({
-        triggers = {
-          -- Leader triggers
-          { mode = 'n', keys = '<Leader>' },
-          { mode = 'x', keys = '<Leader>' },
+  'echasnovski/mini.nvim',
+  version = false,
+  config = function()
+    require('mini.ai').setup()
+    require('mini.align').setup()
+    require('mini.bracketed').setup()
+    local miniclue = require('mini.clue')
+    miniclue.setup({
+      triggers = {
+        -- Leader triggers
+        { mode = 'n', keys = '<Leader>' },
+        { mode = 'x', keys = '<Leader>' },
 
-          -- Built-in completion
-          { mode = 'i', keys = '<C-x>' },
+        -- Built-in completion
+        { mode = 'i', keys = '<C-x>' },
 
-          -- `g` key
-          { mode = 'n', keys = 'g' },
-          { mode = 'x', keys = 'g' },
+        -- `g` key
+        { mode = 'n', keys = 'g' },
+        { mode = 'x', keys = 'g' },
 
-          -- Marks
-          { mode = 'n', keys = "'" },
-          { mode = 'n', keys = '`' },
-          { mode = 'x', keys = "'" },
-          { mode = 'x', keys = '`' },
+        -- Marks
+        { mode = 'n', keys = "'" },
+        { mode = 'n', keys = '`' },
+        { mode = 'x', keys = "'" },
+        { mode = 'x', keys = '`' },
 
-          -- Registers
-          { mode = 'n', keys = '"' },
-          { mode = 'x', keys = '"' },
-          { mode = 'i', keys = '<C-r>' },
-          { mode = 'c', keys = '<C-r>' },
+        -- Registers
+        { mode = 'n', keys = '"' },
+        { mode = 'x', keys = '"' },
+        { mode = 'i', keys = '<C-r>' },
+        { mode = 'c', keys = '<C-r>' },
 
-          -- Window commands
-          { mode = 'n', keys = '<C-w>' },
+        -- Window commands
+        { mode = 'n', keys = '<C-w>' },
 
-          -- `z` key
-          { mode = 'n', keys = 'z' },
-          { mode = 'x', keys = 'z' },
-        },
+        -- `z` key
+        { mode = 'n', keys = 'z' },
+        { mode = 'x', keys = 'z' },
+      },
 
-        clues = {
-          -- Enhance this by adding descriptions for <Leader> mapping groups
-          miniclue.gen_clues.builtin_completion(),
-          miniclue.gen_clues.g(),
-          miniclue.gen_clues.marks(),
-          miniclue.gen_clues.registers(),
-          miniclue.gen_clues.windows(),
-          miniclue.gen_clues.z(),
-        },
-      })
-    end,
-  },
-  {
-    'echasnovski/mini.extra',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.files',
-    config = function()
-      require('mini.files').setup()
-      vim.keymap.set('n', '-', ':lua MiniFiles.open()<cr>', { desc = 'File Browser' } )
-    end,
-    version = false
-  },
-  {
-    'echasnovski/mini.operators',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.pairs',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.starter',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.statusline',
-    config = true,
-    version = false
-  },
-  {
-    'echasnovski/mini.surround',
-    config = true,
-    version = false
-  },
+      clues = {
+        -- Enhance this by adding descriptions for <Leader> mapping groups
+        miniclue.gen_clues.builtin_completion(),
+        miniclue.gen_clues.g(),
+        miniclue.gen_clues.marks(),
+        miniclue.gen_clues.registers(),
+        miniclue.gen_clues.windows(),
+        miniclue.gen_clues.z(),
+      },
+    })
+
+    require('mini.extra').setup()
+
+    require('mini.icons').setup()
+
+    require('mini.files').setup()
+    vim.keymap.set('n', '-', ':lua MiniFiles.open()<cr>', { desc = 'File Browser' } )
+
+    require('mini.operators').setup()
+    require('mini.pairs').setup()
+    require('mini.starter').setup()
+    require('mini.statusline').setup()
+    require('mini.surround').setup()
+  end,
 }
