d539b551af159e5075791655e5aba1c51e4d6545
Tracy Brown Jr <tracybrownjr@gmail.com>
2025-03-31 00:07:29 -0500

    Remove mini.clue, bracketed, and changed some keys

--- lua/plugins/mini.lua
+++ lua/plugins/mini.lua
@@ -2,62 +2,29 @@ return {
   'echasnovski/mini.nvim',
   version = false,
   config = function()
-    require('mini.ai').setup()
-    require('mini.align').setup()
-    require('mini.bracketed').setup()
-    local miniclue = require('mini.clue')
-    miniclue.setup({
-      triggers = {
-        -- Leader triggers
-        { mode = 'n', keys = '<Leader>' },
-        { mode = 'x', keys = '<Leader>' },
+    require('mini.extra').setup()
 
-        -- Built-in completion
-        { mode = 'i', keys = '<C-x>' },
-
-        -- `g` key
-        { mode = 'n', keys = 'g' },
-        { mode = 'x', keys = 'g' },
-
-        -- Marks
-        { mode = 'n', keys = "'" },
-        { mode = 'n', keys = '`' },
-        { mode = 'x', keys = "'" },
-        { mode = 'x', keys = '`' },
-
-        -- Registers
-        { mode = 'n', keys = '"' },
-        { mode = 'x', keys = '"' },
-        { mode = 'i', keys = '<C-r>' },
-        { mode = 'c', keys = '<C-r>' },
-
-        -- Window commands
-        { mode = 'n', keys = '<C-w>' },
-
-        -- `z` key
-        { mode = 'n', keys = 'z' },
-        { mode = 'x', keys = 'z' },
-      },
-
-      clues = {
-        -- Enhance this by adding descriptions for <Leader> mapping groups
-        miniclue.gen_clues.builtin_completion(),
-        miniclue.gen_clues.g(),
-        miniclue.gen_clues.marks(),
-        miniclue.gen_clues.registers(),
-        miniclue.gen_clues.windows(),
-        miniclue.gen_clues.z(),
+    local gen_ai_spec = require('mini.extra').gen_ai_spec
+    require('mini.ai').setup({
+      custom_textobjects = {
+        D = gen_ai_spec.diagnostic(),
+        I = gen_ai_spec.indent(),
+        L = gen_ai_spec.line(),
+        N = gen_ai_spec.number(),
       },
     })
 
-    require('mini.extra').setup()
-
+    require('mini.align').setup()
     require('mini.icons').setup()
 
     require('mini.files').setup()
     vim.keymap.set('n', '-', ':lua MiniFiles.open()<cr>', { desc = 'File Browser' } )
 
-    require('mini.operators').setup()
+    require('mini.operators').setup({
+      replace = {
+        prefix = '<leader>gr',
+      },
+    })
     require('mini.pairs').setup()
     require('mini.starter').setup()
     require('mini.statusline').setup()
