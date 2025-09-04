9b508d9b6d8985b8101aa1e9729fd2acfc330c7e
Tracy Brown <tracy.m.brown@navicogroup.com>
2025-09-04 14:59:23 -0500

    Duplicate code and update packages keymap

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,26 +1,17 @@
-if vim.g.vscode then
-  vim.keymap.set('n', '<c-l>', '<cmd>noh<cr>', { desc = 'No Highlight' })
+vim.keymap.set('n', '<c-l>', '<cmd>noh<cr>', { desc = 'No Highlight' })
 
-  vim.keymap.set('n', '<leader>xG', '<cmd>source %<cr>', { desc = 'Source file' })
-  vim.keymap.set({'n', 'v'}, '<leader>xl', ':.lua<cr>', { desc = 'Lua execute line' })
+vim.keymap.set('n', '<leader>xG', '<cmd>source %<cr>', { desc = 'Source file' })
+vim.keymap.set({'n', 'v'}, '<leader>xl', ':.lua<cr>', { desc = 'Lua execute line' })
 
-  vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
-  vim.keymap.set({'n', 'v'}, '<leader>w', ':w<cr>', { desc = 'Save file' })
+vim.keymap.set('n', '<leader>u', vim.pack.update, { desc = 'Update packages' })
 
-  vim.keymap.set({'n', 'x', 'o'}, '<leader>sj', function() require("flash").jump() end, { desc = 'Flash jump' })
-  vim.keymap.set({'n', 'x', 'o'}, '<leader>sJ', function() require("flash").treesitter() end, { desc = 'Flash treesitter' })
-else
-  vim.keymap.set('n', '<c-l>', '<cmd>noh<cr>', { desc = 'No Highlight' })
+vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
+vim.keymap.set({'n', 'v'}, '<leader>w', ':w<cr>', { desc = 'Save file' })
 
-  vim.keymap.set('n', '<leader>xG', '<cmd>source %<cr>', { desc = 'Source file' })
-  vim.keymap.set({'n', 'v'}, '<leader>xl', ':.lua<cr>', { desc = 'Lua execute line' })
+vim.keymap.set({'n', 'x', 'o'}, '<leader>sj', function() require("flash").jump() end, { desc = 'Flash jump' })
+vim.keymap.set({'n', 'x', 'o'}, '<leader>sJ', function() require("flash").treesitter() end, { desc = 'Flash treesitter' })
 
-  vim.keymap.set({'n', 'v'}, '<leader>y', '"+y', { desc = 'Yank to system clipboard' })
-  vim.keymap.set({'n', 'v'}, '<leader>w', ':w<cr>', { desc = 'Save file' })
-
-  vim.keymap.set({'n', 'x', 'o'}, '<leader>sj', function() require("flash").jump() end, { desc = 'Flash jump' })
-  vim.keymap.set({'n', 'x', 'o'}, '<leader>sJ', function() require("flash").treesitter() end, { desc = 'Flash treesitter' })
-
+if not vim.g.vscode then
   vim.keymap.set('n', '-', ':lua MiniFiles.open()<cr>', { desc = 'File Browser' } )
   local rhs = '<Cmd>lua MiniGit.show_at_cursor()<CR>'
   vim.keymap.set({ 'n', 'x' }, '<Leader>gs', rhs, { desc = 'Show at cursor' })
