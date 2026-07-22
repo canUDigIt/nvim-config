706a2ab88401cbaa0c0f961442c75c3ca5326275
Tracy Brown <tracy.m.brown@navicogroup.com>
2026-07-22 15:09:26 -0500

    Adding in the buffer text object

--- lua/packages.lua
+++ lua/packages.lua
@@ -34,6 +34,7 @@ require('mini.extra').setup()
 local gen_ai_spec = require('mini.extra').gen_ai_spec
 require('mini.ai').setup({
   custom_textobjects = {
+    B = gen_ai_spec.buffer(),
     D = gen_ai_spec.diagnostic(),
     I = gen_ai_spec.indent(),
     L = gen_ai_spec.line(),
